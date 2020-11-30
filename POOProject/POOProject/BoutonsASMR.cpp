d4a43af1261e6e7f5cbabc67ea6f5291c2a19196
BaptisteBurguiere <72495293+BaptisteBurguiere@users.noreply.github.com>
2020-11-30 17:41:15 +0100

    boutons

--- POOProject/POOProject/BoutonsASMR.cpp
+++ POOProject/POOProject/BoutonsASMR.cpp
@@ -1,1 +1,20 @@
 #include "BoutonsASMR.h"
+#include<string>
+
+using namespace std;
+
+BoutonsASMR::BoutonsASMR(string nrequete) {
+
+	b_adrBDD = "on verra";
+	b_requete = nrequete;
+}
+
+void BoutonsASMR::Action() {
+
+
+}
+
+void BoutonAjouter::Action() {
+
+
+}
--- POOProject/POOProject/BoutonsASMR.h
+++ POOProject/POOProject/BoutonsASMR.h
@@ -1,9 +1,37 @@
+#pragma once
+
 #ifndef DEF_BOUTONSASMR
 #define DEF_BOUTONSASMR
 
-#pragma once
-ref class BoutonsASMR
+#include<string>
+
+using namespace std;
+
+class BoutonsASMR
 {
+public:
+
+	BoutonsASMR(string nrequete);
+	virtual void Action();
+
+
+protected:
+
+	string b_requete;
+	string b_adrBDD;
+
+};
+
+
+class BoutonAjouter : public BoutonsASMR {
+
+public:
+
+	void Action();
+
+
+protected:
+
 };
 
 
